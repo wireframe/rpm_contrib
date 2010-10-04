002ce30384bb15355ee75c80bb8921f5b60679d2
Ryan Sonnek <ryan@socialcast.com>
2010-10-03 21:03:36 -0500

    merge backgrounded trace into standard resque instrumentation

--- lib/rpm_contrib/instrumentation/resque.rb
+++ lib/rpm_contrib/instrumentation/resque.rb
@@ -13,14 +13,34 @@ module RPMContrib
         old_perform_method = instance_method(:perform)
 
         define_method(:perform) do
-          class_name = (payload_class ||self.class).name
           NewRelic::Agent.reset_stats if NewRelic::Agent.respond_to? :reset_stats
-          perform_action_with_newrelic_trace(:name => 'perform', :class_name => class_name,
-                                             :category => 'OtherTransaction/ResqueJob') do
+          perform_action_with_newrelic_trace(trace_options) do
             old_perform_method.bind(self).call
           end
 
           NewRelic::Agent.shutdown unless defined?(::Resque.before_child_exit)
+        end
+
+        private
+        def backgrounded_job?
+          if defined?(::Backgrounded::Handler::ResqueHandler) && self.is_a?(::Backgrounded::Handler::ResqueHandler)
+        end
+        def trace_options
+          if backgrounded_job?
+            {
+              :class_name => args[0],
+              :name => args[2].to_s,
+              :params => @payload,
+              :category => 'OtherTransaction/BackgroundedResqueJob'
+            }
+          else
+            class_name = (payload_class ||self.class).name
+            {
+              :class_name => class_name,
+              :name => 'perform',
+              :category => 'OtherTransaction/ResqueJob'
+            }
+          end
         end
       end
 
